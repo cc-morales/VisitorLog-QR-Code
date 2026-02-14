41d4a719f97a3681b8d11b1e47768e8005c58631
Cyrille <cyrilleconm@gmail.com>
2026-02-14 10:02:19 +0800

    migrations

---
+++ VisitorLog/Migrations/20260214020122_entrytype.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VisitorLog.Migrations
+{
+    /// <inheritdoc />
+    public partial class entrytype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "logs",
+                newName: "Timestamp");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EntryType",
+                table: "logs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EntryType",
+                table: "logs");
+
+            migrationBuilder.RenameColumn(
+                name: "Timestamp",
+                table: "logs",
+                newName: "DateCreated");
+        }
+    }
+}
